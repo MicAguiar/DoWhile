aa3310ca7dada9e703a0cd5083b3a898c9689673
MicAguiar <133906973+MicAguiar@users.noreply.github.com>
2023-05-30 18:30:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let numero
+do{
+    numero =  Number(prompt("Numero:"))
+
+}while(numero < 1000)
+
+
+// dowhile faz primeiro uma verificação e depois faz uma condição. Já no While tem primeiro a condição e depois faz a verificação.
