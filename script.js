44558c79389da1de72267e9fe27d205783a028dd
MicAguiar <133906973+MicAguiar@users.noreply.github.com>
2023-06-07 17:15:16 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let numero
-do{
-    numero =  Number(prompt("Numero:"))
-
-}while(numero < 1000)
-
-
-// dowhile faz primeiro uma verificação e depois faz uma condição. Já no While tem primeiro a condição e depois faz a verificação.
